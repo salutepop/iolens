1a05f1cdc6c181061a537c1870680a91047b6507
Changmin <salutepop@gmail.com>
2023-11-17 00:12:39 +0900

    Summary에 Text line 추가 하는 것 구현   - Text line 배열로 추가하면, 자동으로 간격맞춰서 삽입 됨   - Throughput / Latency percentile / Avg. Queuedepth 계산하고 있으나     필요한 항목 더 고민해봐야함.   - Brush 없는 구간 없도록 변경하면 좋을 것 같음 (NaN 문제 해결)     NaN 문제 때문에, toFixed()가 아닌 round * 100 / 100 으로 소수점 잘라냄

--- src/components/SummaryView.js
+++ src/components/SummaryView.js
@@ -1,67 +1,83 @@
-import React, { useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
 import Histogramplot from "./Histogramplot";
 const SummaryView = (props) => {
     const summarySvg = useRef(null);
-    const svgMargin = 20;
-    const svgHeight = 200 + svgMargin;
-    const svgWidth = 200 + svgMargin;
+    const svgMargin = 30;
+    const textWidth = 250;
+    const svgHeight = textWidth + svgMargin * 2;
+    const svgWidth = textWidth + svgMargin * 2;
     const brushedData = props.brushedData;
-    
     const lineHeight = 30;
-    
-    // console.log(data)
+
     useEffect(() => {
-        let textlines = [];
-        // console.log(data)
-        let avgQueue, avgLatency;
-        // data.map(d=>console.log(d))
-        // console.log(data.map(d=>d.idx))
-        // avgQueue = d3.mean(data.map(d=>d.queue_cnt))
-        // avgLatency = d3.mean(data.map(d=>d.latency))
-        textlines.push(`avgQueue: ${avgQueue}`)
-        textlines.push(`avgLatency: ${avgLatency}`)
-        
-        // console.log(avgQueue, avgLatency)
-        const svg = d3.select(summarySvg.current);
-        svg.append('p')
-        /*
-        data.forEach(d => {
-            d.time = parseFloat(d.time);
-            d.queue = parseFloat(d.queue_depth);
-        });
-        */
-        
+        const textlines = [];
+
+        brushedData.map(d => console.log(d))
+
+        // KB/s -> MB/s
+        let throughput = d3.sum(brushedData.map(d => d.size))
+            / (d3.max(brushedData.map(d => d.comp_time))
+                - d3.min(brushedData.map(d => d.comp_time)))
+            / 1024
+
+        // us -> ms
+        let latency_95 = d3.quantile(brushedData.map(d => d.latency), 0.95)
+            / 1000
+        let latency_99 = d3.quantile(brushedData.map(d => d.latency), 0.99)
+            / 1000
+        let latency_9999 = d3.quantile(brushedData.map(d => d.latency), 0.9999)
+            / 1000
+
+        let avgQueue = d3.mean(brushedData.map(d => d.queue_cnt))
+
+        textlines.push(`Throughput: ${Math.round(throughput * 100) / 100} MB/s`)
+        textlines.push(`Lat-95.00%: ${Math.round(latency_95 * 100) / 100} ms`)
+        textlines.push(`Lat-99.00%: ${Math.round(latency_99 * 100) / 100} ms`)
+        textlines.push(`Lat-99.99%: ${Math.round(latency_9999 * 100) / 100} ms`)
+        textlines.push(`Avg. Queue: ${Math.round(avgQueue * 100) / 100}`)
+
+        d3.select(summarySvg.current)
+            .selectAll('text')
+            .data(textlines)
+            .join(
+                enter => enter
+                    .append('text')
+                    .text(textline => textline)
+                    .attr('x', textWidth * 0.1)
+                    .attr('y', (textline, i) => {
+                        return lineHeight * i + svgMargin;
+                    })
+                    .attr('fill', 'black')
+                    .attr('text-anchor', 'start')
+                    .attr('textbaseline', 'bottom')
+                    .attr('font-size', '22px'),
+                update => update
+                    .text(textline => textline),
+                exit => exit.remove(),
+            )
+
+
         const summaryX = 0;
         const summaryY = 0;
 
-        svg.append('rect')
+        d3.select(summarySvg.current)
+            .append('rect')
             .attr('x', summaryX)
             .attr('y', summaryY)
             .attr('width', svgWidth)
             .attr('height', svgHeight)
-            .style('fill', 'white')
-            .style('stroke', 'gray');
-/*
-        const avgQueueDepth = d3.mean(data, d => d.queue);
-
-        svg.append('text')
-            // .attr('x', width/2)
-            // .attr('y', svgHeight/2)
-            .attr('text-anchor', 'middle')
-            .attr('alignment-baseline', 'middle')
-            .style('fill', 'black')
-            .style('textAlign', 'center')
-            .text(`Avg Depth: ${avgQueueDepth.toFixed(2)}`);
-*/
+            .style('fill', 'none')
+            .style('stroke', 'black');
+   
     }, [brushedData]);
 
     return (
         <div>
             <svg ref={summarySvg} width={svgWidth} height={svgHeight}>
             </svg>
-            <Histogramplot brushedData={brushedData}/>
+            <Histogramplot brushedData={brushedData} />
         </div>
     )
 };
