bd3ba7650e18d8d0c60388602e4b1db3cb353afb
Changmin <salutepop@gmail.com>
2023-11-21 11:39:58 +0900

    bind brushedData to histogramPlot.     - bind Data and join the x/y Axis.

--- src/components/Histogramplot.js
+++ src/components/Histogramplot.js
@@ -15,22 +15,33 @@ const Histogramplot = (props) => {
     const svgWidth = plotSize + svgMargin * 2;
     const canvasHist = useRef(null);
 
-    const latencyArray = data.map(d=>d.latency / 1000);
     let nbin = 100;
 
     useEffect(() => {
+        const latencyArray = props.brushedData.map(d => d.latency / 1000);
+        console.log(props.brushedData)
         const maxLatency = d3.max(latencyArray)
 
         // console.log(d3.min(latencyArray))
         let xScale = d3.scaleLinear()
-                        .domain([0, maxLatency])
-                        .range([svgMargin, plotSize + svgMargin]);
+            .domain([0, maxLatency])
+            .range([svgMargin, plotSize + svgMargin]);
         xScale.ticks(nbin);
-        
+
         d3.select(svgHist.current)
             .append('g')
             .attr('transform', `translate(${0},${plotSize + svgMargin})`)
-            .call(d3.axisBottom(xScale));
+            .attr('class', 'xAxis');
+
+        d3.select(svgHist.current)
+            .select('.xAxis')
+            .join(
+                enter => enter,
+                update => update
+                    .call(d3.axisBottom(xScale)),
+                exit => exit.remove(),
+            )
+
         let hist = d3.bin()
             .value(d => d)
             .domain(xScale.domain())
@@ -38,39 +49,52 @@ const Histogramplot = (props) => {
 
         let bins = hist(latencyArray);
         let yScale = d3.scaleLinear()
-                        .domain([0, d3.max(bins.map(d=>d.length))])
-                        .range([plotSize, 0]);
+            .domain([0, d3.max(bins.map(d => d.length))])
+            .range([plotSize, 0]);
+
         d3.select(svgHist.current)
             .append('g')
             .attr('transform', `translate( ${svgMargin}, ${svgMargin})`)
-            .call(d3.axisLeft(yScale));
-        
+            .attr('class', 'yAxis');
+
         d3.select(svgHist.current)
-            .append('g')
+            .select('.yAxis')
+            .join(
+                enter => enter,
+                update => update
+                    .call(d3.axisLeft(yScale)),
+                exit => exit.remove(),
+            );
+
+        d3.select(svgHist.current)
+            // .append('g')
             .selectAll('rect')
             .data(bins)
             .join(
                 enter => enter
-                            .append('rect')
-                            .attr('x', 0)
-                            .attr('y', svgMargin)
-                            .attr('transform', d => `translate(${xScale(d.x0)},${yScale(d.length)})`)
-                            .attr('width', d => xScale(d.x1) - xScale(d.x0))
-                            .attr('height', d => plotSize - yScale(d.length))
-                            .style('fill', 'blue'),
-                update => update,
+                    .append('rect')
+                    .attr('x', 0)
+                    .attr('y', svgMargin)
+                    .attr('transform', d => `translate(${xScale(d.x0)},${yScale(d.length)})`)
+                    .attr('width', d => xScale(d.x1) - xScale(d.x0))
+                    .attr('height', d => plotSize - yScale(d.length))
+                    .style('fill', 'blue'),
+                update => update
+                    .attr('transform', d => `translate(${xScale(d.x0)},${yScale(d.length)})`)
+                    .attr('width', d => xScale(d.x1) - xScale(d.x0))
+                    .attr('height', d => plotSize - yScale(d.length)),
                 exit => exit.remove()
             )
 
-        d3.select(svgHist.current)
-            .append('rect')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('width', svgWidth)
-            .attr('height', svgHeight)
-            .attr('fill', 'none')
-            .attr('stroke', 'red');
-    }, [])
+        // d3.select(svgHist.current)
+        //     .append('rect')
+        //     .attr('x', 0)
+        //     .attr('y', 0)
+        //     .attr('width', svgWidth)
+        //     .attr('height', svgHeight)
+        //     .attr('fill', 'none')
+        //     .attr('stroke', 'black');
+    }, [props.brushedData])
 
     return (
         <div>
@@ -78,7 +102,7 @@ const Histogramplot = (props) => {
             <svg ref={svgHist} height={svgHeight} width={svgWidth}>
 
             </svg>
-            <canvas ref={canvasHist} height={svgHeight} width={svgWidth}/>
+            <canvas ref={canvasHist} height={svgHeight} width={svgWidth} />
         </div>
     )
 };
