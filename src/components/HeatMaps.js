0ac80d308c0e540bd8a823b9903c1a6c40578643
Hyolim <hyolim98@snu.ac.kr>
2023-11-30 15:02:06 +0900

    heatmap

--- src/components/HeatMaps.js
+++ src/components/HeatMaps.js
@@ -21,88 +21,82 @@ const HeatMaps = (props) => {
     useEffect(() => {
 
         const svg = d3.select(splotSvg.current);
-        const data_x = data.map((d) => Object.values(d)[0]);
-        const data_y = data.map((d) => Object.values(d)[1]);
-        const data_value = data.map((d) => Object.values(d)[2]);
+
 
         data.forEach((d, i) => {
 
             d.x = parseFloat(d.sec)
             d.y = parseFloat(d.lba);
             d.value = parseFloat(d.count)
-
+            console.log("d.value", d.value)
 
         })
 
 
 
         let xScale = d3.scaleLinear()
-            .domain([d3.min(data_x), d3.max(data_x)])
+            .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
             .range([0, width]);
-            
+
         let yScale = d3.scaleLinear()
-            .domain([d3.min(data_y), d3.max(data_y)])
+            .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)])
             .range([height, 0]);
 
-        console.log("ymax", d3.max(data_x))
-        console.log("ymin", d3.min(data_x))
 
         svg.append("g")
             .attr('transform', `translate(${margin}, ${height + margin})`)
             .call(d3.axisBottom(xScale))
 
-            
-            let x_min = d3.min(data_x); // data_x 배열에서 최소값
-            let y_min = d3.min(data_y); // data_y 배열에서 최소값
-            
-            // data_x 배열에서 최소값과 동일한 값을 제외한 값을 찾기
-            let x_second_min = d3.min(data_x.filter(value => value !== x_min));
-            let y_second_min = d3.min(data_y.filter(value => value !== y_min));
-            let bandwidth = d3.max(data_x) - d3.min(data_x);
+        let bandwidth = d3.max(data, d => d.x) - d3.min(data, d => d.x);
 
         svg.append("g")
             .attr('transform', `translate(${margin}, ${margin})`)
-            .call(d3.axisLeft(yScale));
-
+            .call(d3.axisLeft(yScale).ticks(10))
+            
 
         const myColor = d3.scaleLinear()
             // .range(["lightgreen", "#69b3a2"])
-            .range(["grey", "lightblue", "blue"])
-            .domain([0, d3.min(data_value) + 1, d3.max(data_value)])
-        
-            console.log("count", d3.min(data_value)+1)
+            .range(["white", "lightblue", "blue"])
+            .domain([0, d3.min(data, d => d.value) + 1, d3.max(data, d => d.value)])
+        // console.log("count", d3.min(data_value)+1)
 
 
         // console.log("height", height/10)
         // console.log("width", width/bandwidth)
-        svg.append("rect")
-        .attr("transform", `translate(${margin + width/bandwidth}, ${margin - (height/10)})`)
-            .attr("x", xScale(1012))
-            .attr("y", yScale(150000000))
-            .attr("width", width/bandwidth)
-            .attr("height", height/10)
-            .attr("fill", "red")
+        
+        
 
         svg.append("g")
-        // .attr("transform", `translate(${margin}, ${margin})`)
-        .attr("transform", `translate(${margin + width/bandwidth}, ${margin - (height/10)})`)
-        .selectAll('rect')
+            // .attr("transform", `translate(${margin}, ${margin})`)
+            .attr("transform", `translate(${margin}, ${margin - (height / 20)})`)
+            .selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
             .attr("x", d => xScale(d.x))
             .attr("y", d => yScale(d.y))
-            .attr("width", width/bandwidth)
-            .attr("height", height/10)
-            // .attr("stroke", "none")
-            .attr("stroke", function (d) { return myColor(d.value)})
+            .attr("width", width / bandwidth)
+            .attr("height", height / 20)
+            .style("stroke-width", "0")
+            .attr("stroke", function (d) { return myColor(d.value) })
+            .attr("opacity", function (d) { if(d.value === 0) return 0})
             .style("fill", function (d) { return myColor(d.value) })
-            // .style("fill", "blue")
+            
 
-        
-            // data.forEach((d, i) => {
-            //    console.log("color", myColor(d.value) )
-            // })
+        // .style("fill", "blue")
+        svg.append("rect")
+        .attr("x", xScale(1050))
+        .attr("y", yScale(2621378))
+        .attr("width", width / 100)
+        .attr("height", height / 20)
+        .attr("fill", "red")
+        .style("stroke-width", "0")
+        // .attr("opacity", "0")
+        .attr("transform", `translate(${margin  }, ${margin - (height / 20)})`)
+
+        // data.forEach((d, i) => {
+        //    console.log("color", d.y )
+        // })
 
     }, []);
 
