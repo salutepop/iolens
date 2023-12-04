58ccaaea7a5262f080923180fe44129cb6be2462
Hyolim <hyolim98@snu.ac.kr>
2023-12-04 18:20:06 +0900

    delete console log

--- src/components/PlotView.js
+++ src/components/PlotView.js
@@ -62,7 +62,7 @@ const PlotView = (props) => {
         }
         ptime = time;
     })
-    console.log("parsedData", parsedData)
+    // console.log("parsedData", parsedData)
     //jw
     // let memFreeScale = d3.scaleLinear()
     //     .domain([d3.min(resource, d => d.mem_free), d3.max(resource, d => d.mem_free)])
